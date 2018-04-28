86731773f233ea98aa178ea74e8f64c339d914fe
IrishWally <10345563@mydbs.ie>
2018-04-28 22:24:14 +0100

    Add files via upload

---
+++ Scripts/js.js
@@ -0,0 +1,27 @@
+﻿$(window).scroll(function () {
+    if ($(document).scrollTop() > 50) {
+        $('#nav').addClass('shrink');
+    }
+    else {
+        $('#nav').removeClass('shrink');
+    }
+});
+
+
+$('#nav').click(function () {
+    console.log("clicked")
+});
+
+    $(function () {
+        $('#signBtn').on("click", function () {
+            console.log("vote clicked ");
+            if ($('#signBtn').text() == "Sign In") {
+                $('#signBtn').text("Menu")
+                alert("sign in")
+            }
+            else if ($('#signBtn').text() == "Menu") {
+                $('#signBtn').text("Sign In");
+                alert("menu")
+            }
+        })
+    });
